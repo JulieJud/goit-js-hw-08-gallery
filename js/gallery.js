e2634c8cf58be84ff94bca0d3ef2b0a7a6a8cede
Julie Judachina <iuliia.saunova@email.cz>
2021-06-08 12:32:26 +0200

    modal close+esc

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,27 @@
 import galleryItems from './gallery-items.js';
 
-const galleryContainer = document.querySelector('.js-gallery');
+const galleryContainer = document.querySelector('ul.js-gallery');
+const lightbox = document.querySelector('.js-lightbox');
+const overlay = document.querySelector('.lightbox__overlay');
+const lightboxImgEl = document.querySelector('img.lightbox__image');
+const btnCloseEl = document.querySelector('button[data-action="close-lightbox"]',);
+
+
+
 const galleryMarkup = makeGalleryMarkup(galleryItems);
+
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+window.addEventListener('keyup', EscapeClick);
+window.addEventListener('click', CloseModalClick);
+
+
 
 function makeGalleryMarkup(images) {
   return images
     .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
+      return`<li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
@@ -23,3 +37,37 @@ function makeGalleryMarkup(images) {
     })
     .join('');
 }
+
+//get url
+
+function onGalleryContainerClick(event) {
+      event.preventDefault();
+  
+   const galleryImage = event.target;
+    if (galleryImage.nodeName !== 'IMG') {
+    return
+
+  }
+  
+  lightboxImgEl.src = event.target.dataset.source;;
+  lightboxImgEl.alt = event.target.alt;
+  lightboxImgEl.dataset.index = event.target.dataset.index;
+  lightbox.classList.add('is-open');  
+ 
+
+};
+
+function EscapeClick (event) {
+  if (event.key === 'Escape') {
+    lightbox.classList.remove('is-open');
+  }
+};
+
+function CloseModalClick (event) {
+  if (event.target.nodeName !== 'IMG') {
+    lightbox.classList.remove('is-open');
+
+    lightboxImgEl.src = '';
+    lightboxImgEl.alt = '';
+  }
+};
